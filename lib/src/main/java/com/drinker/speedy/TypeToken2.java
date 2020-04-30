a3af871be876fa8f4275c480a5b9aea1ca3898ad
Panhao <panhao@52tt.com>
2020-04-30 14:58:35 +0800

    update licence in code

--- lib/src/main/java/com/drinker/speedy/TypeToken2.java
+++ lib/src/main/java/com/drinker/speedy/TypeToken2.java
@@ -1,10 +1,25 @@
+/*
+  Copyright (C) 2008 Google Inc.
+  <p>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 package com.drinker.speedy;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 /**
- * crop from Gson, the destination is get the class type at return or parameter
+ * crop from Gson TypeToken, the destination is get the class type at return or parameter
  *
  * @param <T>
  */
